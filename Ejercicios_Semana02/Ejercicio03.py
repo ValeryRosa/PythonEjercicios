aba108f37e9c0e7aaba81b796dda4f2d411e38ae
ValeryRosa <vrosa.camposs@gmail.com>
2025-08-01 17:19:52 -0500

    Subir más ejercicios

---
+++ Ejercicios_Semana02/Ejercicio03.py
@@ -0,0 +1,7 @@
+
+frutas={"manzana","banana","naranja"}
+numeros={1,2,3,4,5}
+
+colores=set({"rojo","rojo","rojo"})
+vacio=set()
+print(colores)
---
+++ Ejercicios_Semana02/Ejercicio04.py
@@ -0,0 +1,14 @@
+A={1,2,3,4,5}
+B={4,5,6,7,8,9,10}
+#Union de conjuntos
+print(A|B)
+A.union(B)
+
+#Interseccion de conjuntos
+print(A&B)
+
+#Diferencia
+print(A-B)
+
+#Diferencia simétrica
+print(A^B)
